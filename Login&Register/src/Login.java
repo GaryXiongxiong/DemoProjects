93d96220c8d710d487a323c8d4b1f9f9b7d7c6e8
Yixiong Jiang` <i@jiangyixiong.top>
2019-12-07 17:43:42 +0000

    init

---
+++ Login&Register/src/Login.java
@@ -0,0 +1,55 @@
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@WebServlet(name = "login",urlPatterns = "/be/log")
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        String name = request.getParameter("name");
+        String pwd = request.getParameter("pwd");
+        Properties prop=new Properties();
+        InputStream inputStream = Login.class.getClassLoader().getResourceAsStream("druid.properties");
+        Connection conn = null;
+        try {
+            assert inputStream != null;
+            prop.load(inputStream);
+            DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+            conn = dataSource.getConnection();
+            String sql = "SELECT * FROM users WHERE uname = ? AND upwd = MD5(?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,pwd);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {System.out.println("yep");}
+            else {System.out.println("nope");}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      finally{
+            if(conn!=null){
+                try {
+                    conn.close();
+                    System.out.println("closed");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
